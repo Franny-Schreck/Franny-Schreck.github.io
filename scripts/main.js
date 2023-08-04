42ae8de3c03a166c53b8813807fcc4d5908544a5
Franny-Schreck <62480460+Franny-Schreck@users.noreply.github.com>
2023-08-05 00:14:46 +0200

    Get homepage working
    Working examples of homepage and about-page also for phones and small screens, added crisp favicon.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 
 const myImage = document.querySelector("img");
 
+// Change image on click
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/small_focus.jpg") {
@@ -10,6 +11,8 @@ myImage.onclick = () => {
   }
 };
 
+/*
+// Create individual greeting for user
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
@@ -23,7 +26,6 @@ function setUserName() {
     }
   }
   
-
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
@@ -34,3 +36,28 @@ if (!localStorage.getItem("name")) {
 myButton.onclick = () => {
     setUserName();
   };
+
+
+  // Dynamic list with user-generated items
+  const list = document.createElement('ul');
+  const info = document.createElement('p');
+  const html = document.querySelector('html');
+
+  info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+
+  document.body.appendChild(info);
+  document.body.appendChild(list);
+
+  html.onclick = function() {
+    const listItem = document.createElement('li');
+    const listContent = prompt('What content do you want the list item to have?');
+    listItem.textContent = listContent;
+    list.appendChild(listItem);
+
+    listItem.onclick = function(e) {
+      e.stopPropagation();
+      const listContent = prompt('Enter new content for your list item');
+      this.textContent = listContent;
+    }
+  }
+*/
