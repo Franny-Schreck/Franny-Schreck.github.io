7650aff79eaa6fd33b06e3ec24248acf87708cc8
Franny-Schreck <62480460+Franny-Schreck@users.noreply.github.com>
2023-08-07 01:07:03 +0200

    Create Projects page
    Project cards on projects page with content, buttons on homepage cards.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,37 @@
+import hyphenopoly from "hyphenopoly";
+
+// AOS library initialisation and duration setting
+
 AOS.init({
   duration: 500,
 })
 
+// Load and install the Hyphenopoly library 
+
+const Hyphenopoly = {
+    "require": {
+        "es": "anticonstitucionalmente",
+        "it": "precipitevolissimevolmente",
+        "de": "Silbentrennungsalgorithmus",
+        "en-us": "antidisestablishmentarianism"
+    },
+    "paths": {
+        // Path to the directory of pattern files
+        "patterndir": "./js/hyphenopoly/patterns/",
+        // Path to the directory where the other ressources are stored
+        "maindir": "./js/hyphenopoly/"
+    }
+};
+
+hyphenopoly.config({
+  require: {
+      "de": "FORCEHYPHENOPOLY",
+      "en-us": "FORCEHYPHENOPOLY"
+  },
+  setup: {
+      selectors: {
+          ".container": {},
+          ".body": {}
+      }
+  }
+});
