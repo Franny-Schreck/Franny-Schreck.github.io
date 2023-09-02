6548a15c4adbbd540cbf53a52bbad869ed92256a
Franny-Schreck <62480460+Franny-Schreck@users.noreply.github.com>
2023-09-02 18:17:01 +0200

    Fix usage of Hyphenopoly and change lang to en-GB
    Hyphenopoly was previously imported incorrectly via main.js instead of
    directly in the html <head> as documented in Hyphenopoly's README.
    Additionally, Hyphenopoly's wasm engine files were previously not
    included, which - somehow - did not break it. This has also been fixed
    now.
    The html lang attribute has also been changed from en-US to en-GB to
    reflect the actually used spelling. This is also necessary to allow
    Hyphenopoly to recognize the text's language.

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,4 @@
-
 // AOS library initialisation and duration setting
-
 AOS.init({
   duration: 500,
 })
-
-
-// Load and install the Hyphenopoly library 
-
-import hyphenopoly from "hyphenopoly";
-
-
-const Hyphenopoly = {
-    "require": {
-        "es": "anticonstitucionalmente",
-        "it": "precipitevolissimevolmente",
-        "de": "Silbentrennungsalgorithmus",
-        "en-us": "antidisestablishmentarianism"
-    },
-    "paths": {
-        // Path to the directory of pattern files
-        "patterndir": "./js/hyphenopoly/patterns/",
-        // Path to the directory where the other ressources are stored
-        "maindir": "./js/hyphenopoly/"
-    }
-};
-
-hyphenopoly.config({
-  require: {
-      "de": "FORCEHYPHENOPOLY",
-      "en-us": "FORCEHYPHENOPOLY"
-  },
-  setup: {
-      selectors: {
-          ".container": {},
-          ".body": {}
-      }
-  }
-});
