5b483bcad1301cadc9d2d09ca5081155ed8622c7
Franny-Schreck <62480460+Franny-Schreck@users.noreply.github.com>
2023-09-28 15:06:30 +0200

    Create and implement own animate on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,48 @@
-// AOS library initialisation and duration setting
-AOS.init({
-  duration: 500,
-})
+/**
+ * Percentage threshold above which an element is considered on-screen and
+ * below which it is considered off-screen.
+ * The percentage does NOT refer to the amount of the element on screen.
+ * Instead it refers to the percentage of the screen that is below the
+ * element's top edge.
+ */
+const perc_threshold = 0.08;
+
+/**
+ * Calculates the percentage of the screen that is below the passed HTML
+ * Element's top edge.
+ * Any vertical translation is ignored, meaning that the percentage is
+ * calculated as if the element were untranslated.
+ * 
+ * @param {Element} elem
+ * @returns Percentage of the screen that is below elem's top edge, in an
+ *          endpoint-inclusive range from 0 to 1
+ */
+const filled_perc = (elem) => {
+        // Account for vertical translation.
+        const y_offset = new DOMMatrixReadOnly(window.getComputedStyle(elem).transform).m42;
+
+        const perc = window.innerHeight - elem.getBoundingClientRect().top + y_offset;
+
+        // Clamp to interval [0, 1].
+        return perc < 0 ? 0 : perc > window.innerHeight ? 1 : perc / window.innerHeight;
+}
+
+/**
+ * Determines whether an element is considered on-screen or off-screen, and
+ * sets or removes the element's on-screen class accordingly.
+ * 
+ * @param {Element} elem
+ */
+const handle_on_screen = (elem) => {
+        if (filled_perc(elem) > perc_threshold)
+                elem.classList.add('on-screen');
+        else
+                elem.classList.remove('on-screen');
+};
+
+// Initialize on-screen class depending on the initial location of elements
+// when the page is loaded.
+document.querySelectorAll('.faded').forEach((elem) => handle_on_screen(elem));
+
+// Recalculate on-screen class on every scroll.
+document.addEventListener('scroll', (_) => document.querySelectorAll('.faded').forEach((elem) => handle_on_screen(elem)));
